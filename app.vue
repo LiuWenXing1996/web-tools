21b940d3edfa97ee5348b5865737921993ec5fe7
柳问星 <lwx_redstone@163.com>
2024-10-29 01:58:00 +0800

    feat: add element-plus pacakge

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div id="app">
+    <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
