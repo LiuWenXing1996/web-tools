9842c2dea8f5ceeee16a12e19bbfe392a4f718b0
柳问星 <lwx_redstone@163.com>
2024-11-11 23:56:09 +0800

    fix: 修复页面显示错误

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     baseURL: "/web-tools/",
+    buildAssetsDir: "/static/",
   },
   modules: [
     "@element-plus/nuxt",
